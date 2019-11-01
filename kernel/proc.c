02e60952c99af0c9c215a845bae5a1720d7505b4
Jason Lam <jasonlam@mit.edu>
2019-11-01 01:46:26 -0400

    properly init jid to -1

--- kernel/proc.c
+++ kernel/proc.c
@@ -38,6 +38,7 @@ procinit(void)
       uint64 va = KSTACK((int) (p - proc));
       kvmmap(va, (uint64)pa, PGSIZE, PTE_R | PTE_W);
       p->kstack = va;
+      p->jid = -1;
   }
   kvminithart();
 }
