54007cba4840930b4510ce6193ba77ee3d5462af
Louisa Huang <rlhuang@mit.edu>
2019-12-06 22:19:20 -0500

    completely remove inode limit-related things

--- kernel/jail.h
+++ kernel/jail.h
@@ -5,8 +5,6 @@ struct jail {
   uint64 memusage; 
   uint64 disklim; 
   uint64 diskusage;
-  uint64 inodelim;  // not sure how to keep track of usage 
-  uint64 inodeusage;
   struct inode *rootdir;
   uint64 numproc;
   uint64 maxproc; 
